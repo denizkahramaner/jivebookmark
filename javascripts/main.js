2bc828dc69e8ba4d8f51c3a74f0484852d875c55
Akshay Narayan <akshay.narayan@jivesoftware.com>
2013-08-16 16:17:18 -0700

    testing

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,22 +1,10 @@
 gadgets.util.registerOnLoadHandler(function() {
-  // add code that should run on page load here
 
-  // resize app window to fit content
-  // gadgets.window.adjustHeight();
-//    var object = {
-//        'href': 'https://brewspace.jiveland.com/bookmarks?itemView=detail',
-//        'noCache': true,
-//    }
-//    osapi.http.get(object).execute(function(response) {
-//        console.log("response",response);
-//    });
     $.get('/bookmarks?itemView=detail', function(data) {
 //        console.log('success', data);
         var el = $('<div></div>');
         el.html(data);
-        $("tbody .j-browse-details-body").find("td .j-td-title").forEach(function(row) {
-            console.log(row);
-        });
-
+        var res = $("tbody .j-browse-details-body").find("td .j-td-title")
+        console.log(res);
     });
 });
